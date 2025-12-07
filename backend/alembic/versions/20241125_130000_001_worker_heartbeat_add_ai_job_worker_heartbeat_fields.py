4ab6c9f2908328cfcaeccbd38d0958123ef4d6a1
Chris Rosales <10rosachri@gmail.com>
2025-12-06 21:00:41 -0500

    Add initial database schema migration

--- backend/alembic/versions/20241125_130000_001_worker_heartbeat_add_ai_job_worker_heartbeat_fields.py
+++ backend/alembic/versions/20241125_130000_001_worker_heartbeat_add_ai_job_worker_heartbeat_fields.py
@@ -1,7 +1,7 @@
 """add ai job worker heartbeat fields
 
 Revision ID: 001_worker_heartbeat
-Revises:
+Revises: 001_initial_schema
 Create Date: 2024-11-25 13:00:00.000000
 
 """
@@ -15,7 +15,7 @@ from alembic import op
 
 # revision identifiers, used by Alembic.
 revision: str = "001_worker_heartbeat"
-down_revision: Union[str, None] = None
+down_revision: Union[str, None] = "001_initial_schema"
 branch_labels: Union[str, Sequence[str], None] = None
 depends_on: Union[str, Sequence[str], None] = None
 

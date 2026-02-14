fb136469666334fb03b0c5e293163584b6dff5ca
Chris Rosales <10rosachri@gmail.com>
2026-02-14 12:31:26 -0500

    desktop(editor): phase-27 extract inspector declaration cluster

---
+++ desktop/BeatSight.Game/Screens/Editor/EditorScreen.InspectorState.cs
@@ -0,0 +1,62 @@
+using System.Collections.Generic;
+using BeatSight.Game.UI.Components;
+using osu.Framework.Bindables;
+using osu.Framework.Graphics;
+using osu.Framework.Graphics.Containers;
+using osu.Framework.Graphics.Sprites;
+using osu.Framework.Graphics.UserInterface;
+using SpriteText = BeatSight.Game.UI.Components.BeatSightSpriteText;
+
+namespace BeatSight.Game.Screens.Editor
+{
+    public partial class EditorScreen
+    {
+        private bool inspectorStackedLayout;
+        private bool inspectorCollapsed;
+        private readonly List<BeatSightTextBox> inspectorTextBoxes = new();
+        private readonly List<BasicButton> inspectorActionButtons = new();
+        private readonly List<SpriteText> inspectorActionButtonTexts = new();
+        private readonly List<(BasicButton Button, SpriteText Label, bool FillWidth, float WidthHint)> inspectorActionLayouts = new();
+        private readonly List<BeatSightSliderBar> inspectorSliders = new();
+        private readonly List<FillFlowContainer> inspectorSectionBodies = new();
+        private readonly List<FillFlowContainer> inspectorFieldFlows = new();
+        private readonly List<SpriteText> inspectorSectionTitleTexts = new();
+        private readonly List<SpriteText> inspectorFieldLabelTexts = new();
+        private readonly List<SpriteText> footerKeyTexts = new();
+        private readonly List<SpriteText> footerActionTexts = new();
+        private readonly Dictionary<EditorInspectorSectionKey, Drawable> inspectorSectionsByKey = new();
+        private Drawable inspectorMetadataSection = null!;
+        private Drawable inspectorTimingSection = null!;
+        private Drawable inspectorStatsSection = null!;
+        private Drawable inspectorGenerationSection = null!;
+        private bool inspectorCompactHierarchy;
+
+        private BasicTextBox releaseInput = null!;
+        private BasicTextBox providerInput = null!;
+        private BasicTextBox descriptionInput = null!;
+        private BasicTextBox titleInput = null!;
+        private BasicTextBox artistInput = null!;
+        private BasicTextBox creatorInput = null!;
+        private BasicTextBox sourceInput = null!;
+        private BasicTextBox tagsInput = null!;
+        private BasicTextBox bpmInput = null!;
+        private BasicTextBox offsetInput = null!;
+        private BasicTextBox tempoHintsInput = null!;
+
+        private Bindable<string> quantizationGrid = new Bindable<string>("sixteenth");
+        private BindableDouble maxSnapError = new BindableDouble(12.0) { MinValue = 1.0, MaxValue = 50.0, Precision = 0.1 };
+        private BindableDouble confidenceThreshold = new BindableDouble(0.3) { MinValue = 0.1, MaxValue = 0.9, Precision = 0.01 };
+        private BindableDouble detectionSensitivity = new BindableDouble(60.0) { MinValue = 1.0, MaxValue = 100.0, Precision = 1.0 };
+        private BindableBool isolateDrums = new BindableBool(true);
+        private BindableBool forceQuantization = new BindableBool(false);
+        private BindableBool useMlClassifier = new BindableBool(true);
+
+        private SpriteText noteCountValue = null!;
+        private SpriteText selectionSummaryText = null!;
+        private SpriteText mapLengthValue = null!;
+        private SpriteText densityValue = null!;
+        private SpriteText bpmStatValue = null!;
+        private BeatSight.Game.UI.Components.Dropdown<string> componentReassignDropdown = null!;
+        private readonly Bindable<string> componentReassignSelection = new Bindable<string>("kick");
+    }
+}
--- desktop/BeatSight.Game/Screens/Editor/EditorScreen.cs
+++ desktop/BeatSight.Game/Screens/Editor/EditorScreen.cs
@@ -106,53 +106,7 @@ namespace BeatSight.Game.Screens.Editor
         private float lastStackedInspectorHeight = -1;
         private float lastPanelGap = -1;
         private float lastCompactBlend = -1f;
-        private bool inspectorStackedLayout;
-        private bool inspectorCollapsed;
-        private readonly List<BeatSightTextBox> inspectorTextBoxes = new();
-        private readonly List<BasicButton> inspectorActionButtons = new();
-        private readonly List<SpriteText> inspectorActionButtonTexts = new();
-        private readonly List<(BasicButton Button, SpriteText Label, bool FillWidth, float WidthHint)> inspectorActionLayouts = new();
-        private readonly List<BeatSightSliderBar> inspectorSliders = new();
-        private readonly List<FillFlowContainer> inspectorSectionBodies = new();
-        private readonly List<FillFlowContainer> inspectorFieldFlows = new();
-        private readonly List<SpriteText> inspectorSectionTitleTexts = new();
-        private readonly List<SpriteText> inspectorFieldLabelTexts = new();
-        private readonly List<SpriteText> footerKeyTexts = new();
-        private readonly List<SpriteText> footerActionTexts = new();
-        private readonly Dictionary<EditorInspectorSectionKey, Drawable> inspectorSectionsByKey = new();
-        private Drawable inspectorMetadataSection = null!;
-        private Drawable inspectorTimingSection = null!;
-        private Drawable inspectorStatsSection = null!;
-        private Drawable inspectorGenerationSection = null!;
-        private bool inspectorCompactHierarchy;
-
-        private BasicTextBox releaseInput = null!;
-        private BasicTextBox providerInput = null!;
-        private BasicTextBox descriptionInput = null!;
-        private BasicTextBox titleInput = null!;
-        private BasicTextBox artistInput = null!;
-        private BasicTextBox creatorInput = null!;
-        private BasicTextBox sourceInput = null!;
-        private BasicTextBox tagsInput = null!;
-        private BasicTextBox bpmInput = null!;
-        private BasicTextBox offsetInput = null!;
-
-        private Bindable<string> quantizationGrid = new Bindable<string>("sixteenth");
-        private BindableDouble maxSnapError = new BindableDouble(12.0) { MinValue = 1.0, MaxValue = 50.0, Precision = 0.1 };
-        private BindableDouble confidenceThreshold = new BindableDouble(0.3) { MinValue = 0.1, MaxValue = 0.9, Precision = 0.01 };
-        private BindableDouble detectionSensitivity = new BindableDouble(60.0) { MinValue = 1.0, MaxValue = 100.0, Precision = 1.0 };
-        private BindableBool isolateDrums = new BindableBool(true);
-        private BindableBool forceQuantization = new BindableBool(false);
-        private BindableBool useMlClassifier = new BindableBool(true);
-        private BasicTextBox tempoHintsInput = null!;
-
-        private SpriteText noteCountValue = null!;
-        private SpriteText selectionSummaryText = null!;
-        private SpriteText mapLengthValue = null!;
-        private SpriteText densityValue = null!;
-        private SpriteText bpmStatValue = null!;
-        private BeatSight.Game.UI.Components.Dropdown<string> componentReassignDropdown = null!;
-        private readonly Bindable<string> componentReassignSelection = new Bindable<string>("kick");
+        // Inspector declaration cluster extracted to EditorScreen.InspectorState.cs.
 
         private bool isPlaying;
         private double currentTime;

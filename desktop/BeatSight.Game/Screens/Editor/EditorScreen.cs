fe2d8bd68dc023163b43d18fe4c16ab61f4c1e67
Chris Rosales <10rosachri@gmail.com>
2026-02-13 23:14:33 -0500

    desktop(editor): extract lifecycle hooks into phase-17 partial

--- desktop/BeatSight.Game/Screens/Editor/EditorScreen.cs
+++ desktop/BeatSight.Game/Screens/Editor/EditorScreen.cs
@@ -2370,39 +2370,5 @@ namespace BeatSight.Game.Screens.Editor
             updateInspectorToggle(metrics, viewport);
         }
 
-        public override bool OnExiting(ScreenExitEvent e)
-        {
-            endTimelineZoomInteraction();
-            endWaveformScaleInteraction();
-            uiAudio.PlayBack();
-            stopPlayback(silent: true);
-            disposeTrack();
-            return base.OnExiting(e);
-        }
-
-        protected override void Dispose(bool isDisposing)
-        {
-            base.Dispose(isDisposing);
-            disposeTrack();
-            storageTrackStore?.Dispose();
-            storageTrackStore = null;
-            storageResourceStore?.Dispose();
-            storageResourceStore = null;
-        }
-
-        private partial class PassiveScrollContainer : BeatSightScrollContainer
-        {
-            public PassiveScrollContainer(Direction direction = Direction.Vertical)
-                : base(direction)
-            {
-            }
-
-            protected override bool OnDragStart(DragStartEvent e) => false;
-
-            protected override void OnDrag(DragEvent e)
-            {
-            }
-        }
-
     }
 }

c5c295857207f83f9144146159ef7c7d6b0b5057
Chris Rosales <10rosachri@gmail.com>
2026-02-14 01:26:47 -0500

    desktop(editor): extract initialization lifecycle into phase-23 partial

---
+++ desktop/BeatSight.Game/Screens/Editor/EditorScreen.Initialization.cs
@@ -0,0 +1,166 @@
+using System;
+using BeatSight.Game.Configuration;
+using BeatSight.Game.Screens.Playback.Playfield.Views;
+using BeatSight.Game.UI.Components;
+using BeatSight.Game.UI.Theming;
+using osu.Framework.Allocation;
+using osu.Framework.Bindables;
+using osu.Framework.Graphics;
+using osu.Framework.Graphics.Containers;
+using osu.Framework.Graphics.Shapes;
+using osu.Framework.IO.Stores;
+using osu.Framework.Screens;
+using osuTK;
+
+namespace BeatSight.Game.Screens.Editor
+{
+    public partial class EditorScreen
+    {
+        [BackgroundDependencyLoader]
+        private void load()
+        {
+            storageResourceStore ??= new StorageBackedResourceStore(host.Storage);
+            storageTrackStore ??= audioManager.GetTrackStore(storageResourceStore);
+            laneViewModeBindable = config.GetBindable<LaneViewMode>(BeatSightSetting.LaneViewMode);
+            laneViewMode = laneViewModeBindable.GetBoundCopy();
+            lanePresetSetting = config.GetBindable<LanePreset>(BeatSightSetting.LanePreset);
+            kickLaneModeSetting = config.GetBindable<KickLaneMode>(BeatSightSetting.KickLaneMode);
+
+            // Initialize preview mode based on current setting
+            var initialPreviewMode = EditorPreviewMode.Playfield3D;
+            switch (laneViewModeBindable.Value)
+            {
+                case LaneViewMode.TwoDimensional: initialPreviewMode = EditorPreviewMode.Playfield2D; break;
+                case LaneViewMode.Manuscript: initialPreviewMode = EditorPreviewMode.Manuscript; break;
+                case LaneViewMode.ThreeDimensional: initialPreviewMode = EditorPreviewMode.Playfield3D; break;
+            }
+            previewMode = new Bindable<EditorPreviewMode>(initialPreviewMode);
+
+            editorTimelineZoomDefault = config.GetBindable<double>(BeatSightSetting.EditorTimelineZoomDefault);
+            editorWaveformScaleDefault = config.GetBindable<double>(BeatSightSetting.EditorWaveformScaleDefault);
+            editorBeatGridVisibleDefault = config.GetBindable<bool>(BeatSightSetting.EditorBeatGridVisibleDefault);
+
+            bool previousPersistenceState = suppressEditorDefaultPersistence;
+            suppressEditorDefaultPersistence = true;
+            applyEditorDefaultsFromConfig();
+            suppressEditorDefaultPersistence = previousPersistenceState;
+
+            laneViewMode.BindValueChanged(onLaneViewModeChanged, true);
+            previewMode.BindValueChanged(onPreviewModeChanged);
+
+            backButton = new BackButton
+            {
+                Margin = BackButton.DefaultMargin,
+                Action = () => this.Exit()
+            };
+
+            var editorEdgePadding = new MarginPadding
+            {
+                Left = UITheme.ScreenPadding.Left + 20,
+                Right = UITheme.ScreenPadding.Right + 20,
+                Top = UITheme.ScreenPadding.Top + 12,
+                Bottom = UITheme.ScreenPadding.Bottom + 12
+            };
+
+            var paddedLayout = new ScreenEdgeContainer(scrollable: false)
+            {
+                EdgePadding = editorEdgePadding,
+                Content = screenLayoutGrid = new GridContainer
+                {
+                    RelativeSizeAxes = Axes.Both,
+                    RowDimensions = new[]
+                    {
+                        new Dimension(GridSizeMode.AutoSize),
+                        new Dimension(),
+                        new Dimension(GridSizeMode.Absolute, getInitialFooterHeight())
+                    },
+                    Content = new[]
+                    {
+                        new Drawable[] { createHeader() },
+                        new Drawable[] { createEditor() },
+                        new Drawable[] { createFooter() }
+                    }
+                }
+            };
+
+            var backButtonOverlay = new SafeAreaContainer
+            {
+                RelativeSizeAxes = Axes.Both,
+                Padding = BackButton.DefaultMargin,
+                Child = backButton
+            };
+
+            historyPanel!.Y = editorEdgePadding.Top + 64;
+
+            InternalChildren = new Drawable[]
+            {
+                new Box
+                {
+                    RelativeSizeAxes = Axes.Both,
+                    Colour = EditorColours.ScreenBackground
+                },
+                new Box
+                {
+                    RelativeSizeAxes = Axes.Both,
+                    Height = 0.48f,
+                    Anchor = Anchor.TopLeft,
+                    Origin = Anchor.TopLeft,
+                    Colour = EditorColours.ScreenBackdropTop
+                },
+                new Box
+                {
+                    RelativeSizeAxes = Axes.Both,
+                    Height = 0.52f,
+                    Anchor = Anchor.BottomLeft,
+                    Origin = Anchor.BottomLeft,
+                    Colour = EditorColours.ScreenBackdropBottom.Opacity(0.9f)
+                },
+                paddedLayout,
+                historyPanel!,
+                backButtonOverlay,
+                quickActionToast = new ToastContainer
+                {
+                    RelativeSizeAxes = Axes.Both
+                },
+                createLogOverlay()
+            };
+
+            if (!string.IsNullOrEmpty(beatmapPath))
+            {
+                loadBeatmap(beatmapPath);
+            }
+            else if (importedAudio != null)
+            {
+                initializeNewProject(importedAudio);
+            }
+            else
+            {
+                // Initialize a blank project if no beatmap or audio is provided
+                initializeNewProject(null);
+                reloadTimeline();
+                updateActionButtons();
+                refreshTimelineToolboxState();
+                updatePlaybackAvailabilityUI();
+            }
+        }
+
+        protected override void LoadComplete()
+        {
+            base.LoadComplete();
+
+            showDrumStem.BindValueChanged(_ => updateWaveformSource());
+
+            // Ensure preview is synchronized after everything is loaded
+            if (beatmap != null && playbackPreview != null)
+            {
+                playbackPreview.SetBeatmap(beatmap);
+            }
+
+            // Make sure the correct preview mode is visible
+            onPreviewModeChanged(new ValueChangedEvent<EditorPreviewMode>(previewMode.Value, previewMode.Value));
+
+            updatePlaybackAvailabilityUI();
+            applyResponsiveEditorLayout(force: true);
+        }
+    }
+}
